9d5a03d8e9f30b5f3f4bbde778cb0e1f41b34272
Sherkhan Tulkibay <sherkhan-92@bk.ru>
2022-04-06 15:37:34 +0600

    add One Root state

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -4,9 +4,8 @@ import {Subscription} from "rxjs";
 import {Store} from "@ngrx/store";
 
 import {Ingredient} from "../../shared/ingredient.model";
-import {ShoppingListService} from "../shopping-list.service";
 import * as ShoppingListActions from "../store/shopping-list.actions";
-import * as fromShoppingList from "../store/shopping-list.reducer";
+import * as fromApp from "../../store/app.reducer"
 
 
 @Component({
@@ -20,8 +19,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   editMode = false;
   editedItem: Ingredient;
 
-  constructor(private slService: ShoppingListService,
-              private store: Store<fromShoppingList.AppState>
+  constructor(private store: Store<fromApp.AppState>
   ) {
   }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,13 +1,11 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Subscription, Observable} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 import {Store} from "@ngrx/store";
 
-import { Ingredient } from "../shared/ingredient.model";
-import {ShoppingListService} from "./shopping-list.service";
+import {Ingredient} from "../shared/ingredient.model";
 import {LoggingService} from "../logging.service";
-import * as fromShoppingList from "./store/shopping-list.reducer";
 import * as ShoppingListActions from "./store/shopping-list.actions";
-
+import * as fromApp from "../store/app.reducer";
 
 
 @Component({
@@ -19,9 +17,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Observable<{ingredients: Ingredient[]}>;
   private subscription: Subscription;
 
-  constructor(private slService: ShoppingListService,
-              private loggingService: LoggingService,
-              private store: Store<fromShoppingList.AppState>
+  constructor(private loggingService: LoggingService,
+              private store: Store<fromApp.AppState>
   ) { }
 
   ngOnInit(): void {
